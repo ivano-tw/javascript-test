3574c85219c14d7bec0ea02ebb576f6ee92a85ac
ivanortega <ivan_ka12@outlook.com>
2022-03-09 21:57:37 -0500

    Add script to run non constructible change function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('hello world');
+const { getNonConstructibleChange } = require("./non-constructible-change")
+
+const nonConstructibleChange = () => {
+  const coins = [5, 7, 1, 1, 2, 3, 22];
+  const minimumNonConstructibleChange = getNonConstructibleChange();
+  console.log(`The minimun non constructible change for the coins ${coins} is : ${minimumNonConstructibleChange}`);
+}
+
+const functionName = process.argv.slice(2)[0];
+
+if (functionName === 'change') {
+  nonConstructibleChange();
+} else {
+  console.log('Does not exists');
+}
